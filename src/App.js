ab4ba037831aebddfd5a032816e2542214fc3c8f
Geocarlos Lopes Alves <geowildcat@gmail.com>
2018-02-18 18:56:12 -0400

    Create one BookShelf component for all shelves

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './utils/BooksAPI'
 import './App.css';
 import CurrentlyReading from './components/currently_reading';
 import WantToRead from './components/want_to_read';
 import Read from './components/read';
+import BookShelf from './components/book_shelf';
 import SearchPage from './components/search_page';
 
 class BooksApp extends React.Component {
   state = {
+      books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -17,7 +19,15 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  componentDidMount(){
+    BooksAPI.getAll().then(books =>{
+      this.setState({books: books})
+    });
+  }
+
   render() {
+    console.log(this.state.books)
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -27,9 +37,20 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            <CurrentlyReading />
-            <WantToRead />
-            <Read />
+            <BookShelf
+              bookList={this.state.books.filter(book => book.shelf === "currentlyReading")}
+              name="Currently Reading"
+            />
+
+            <BookShelf
+              bookList={this.state.books.filter(book => book.shelf === "wantToRead")}
+              name="Want To Read"
+            />
+
+            <BookShelf
+              bookList={this.state.books.filter(book => book.shelf === "read")}
+              name="Read"
+            />
 
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
---
+++ src/components/book_shelf.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class BookShelf extends Component{
+
+  render(){
+
+    const { bookList, name } = this.props;
+
+    if(bookList.length > 0) {
+      bookList.map((book, i) => console.log(`${i}: ${book.title}, ${book.imageLinks.thumbnail}`))
+    }
+    if(bookList.length < 1) return (<div>Loading data ...</div>)
+    return (
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">{name}</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {bookList.map((book, i)=>(
+                  <li key={i}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors[0]}</div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf;
