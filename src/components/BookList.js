8b3194bf8a68fb1edd6298d08567992284ad1853
Geocarlos Lopes Alves <geowildcat@gmail.com>
2018-02-22 00:06:07 -0400

    Fix picking first option right away

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,6 +4,9 @@ const BookList = (props) => {
 
   const { bookList, onUpdate } = props;
 
+  // Avoid picking first option right away
+  let chosen = false;
+
   return (<ol className="books-grid">
     {bookList.map(book => (
       <li key={book.id}>
@@ -11,7 +14,9 @@ const BookList = (props) => {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
             <div className="book-shelf-changer">
-              <select onMouseUp={event => {onUpdate(book, event.target.value); console.log(event.target.value)}}>
+              <select onMouseUp={event => {chosen = !chosen; if(!chosen)
+                onUpdate(book, event.target.value); console.log(event.target.value)}}
+                onMouseLeave={()=> chosen = false}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
